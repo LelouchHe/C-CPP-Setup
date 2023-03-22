003b4d188d33d1b13adb3630f863b38113e11c42
longh <lelouchhe@gmail.com>
2023-03-22 10:30:38 -0700

    Setup dev env

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,12 @@
+#include <stdint.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
+void hello(int j) {
+  int i = 0;
+}
 
 int main() {
   int i = 0;
@@ -9,5 +14,6 @@ int main() {
             << std::endl;
   std::vector<int> v = {};
   v.push_back(i);
+  hello(i);
   return 0;
 }
