9e782b2dc862a94113fd445bbb08fe0a0bd07ab0
Lelouch He <lelouchhe@gmail.com>
2023-03-22 21:19:17 -0700

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,6 @@
 #include <iostream>
-#include <vector>
-
-using namespace std;
-
-void hello(int j) {
-  int i = 0;
-  std::string shel;
-  std::unique_ptr<int> p;
-}
 
 int main() {
-  int i = 0;
-  std::cout << "hello worlddddddddddddddddddddddddddddddddddddddddddddddddd"
-            << std::endl;
-  std::vector<int> v = {};
-  v.push_back(i);
-  hello(i);
+  std::cout << "hello world" << std::endl;
   return 0;
 }
